429684f0b5d9d610b9beebb0e2118d72dd96c5a2
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-08-11 19:23:50 -0300

    first commit

---
+++ src/main/java/com/backend/adiministror/AdiministrorApplication.java
@@ -0,0 +1,13 @@
+package com.backend.adiministror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiministrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdiministrorApplication.class, args);
+    }
+
+}
