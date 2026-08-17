cb5d5fc47a3250b61c6b68306a74ac21a6b37421
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-08-17 16:19:54 -0300

    fix: userModel com validação

--- src/main/java/com/backend/adiministror/model/UsuarioModel.java
+++ src/main/java/com/backend/adiministror/model/UsuarioModel.java
@@ -39,4 +39,32 @@ public class UsuarioModel {
         this.phone = phone;
         this.role = Role.USUARIO;
     }
+
+    public void atualizrDados(
+            String nome, String email, String phone
+    ) {
+        this.nome = nome;
+        this.email = email;
+        this.phone = phone;
+    }
+    public void alterarSenha(String senha) {
+        if (senha == null || senha.trim().isEmpty()) {
+            throw new RuntimeException("Senha não pode ser vazia");
+        }
+        if (senha.length() < 8) {
+            throw new RuntimeException("Senha deve ter no minimo 8 caracteres");
+        }
+
+        if (!senhaValida(senha)) throw new RuntimeException("Senha deve conter no mínimo 1 número e 1 caractere especial");
+
+        this.senha = senha;
+    }
+    public static boolean senhaValida(String senha) {
+        if (senha == null || senha.isEmpty()) {
+            return false; // Senha vazia ou nula é inválida
+        }
+        boolean temNumero = senha.matches(".*\\d.*");
+        boolean temEspecial = senha.matches(".*[^\\p{L}\\p{N}\\s].*");
+        return temNumero && temEspecial;
+    }
 }
---
+++ src/main/java/com/backend/adiministror/service/GaleriaService.java
@@ -0,0 +1,4 @@
+package com.backend.adiministror.service;
+
+public class GaleriaService {
+}
