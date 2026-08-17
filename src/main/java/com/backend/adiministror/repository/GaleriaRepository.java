70b36d2ad25f4b52cd1ebaa6c892fcdfa3350605
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-08-17 16:20:31 -0300

    feat: buscar por nome no repository  de galeria

--- src/main/java/com/backend/adiministror/repository/GaleriaRepository.java
+++ src/main/java/com/backend/adiministror/repository/GaleriaRepository.java
@@ -3,9 +3,10 @@ package com.backend.adiministror.repository;
 import com.backend.adiministror.model.GaleriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GaleriaRepository extends JpaRepository<GaleriaModel, UUID> {
-
+    List<GaleriaModel> findByNomeIgnoreCase(String nome);
     long countBySalaId(UUID salaId);
 }
