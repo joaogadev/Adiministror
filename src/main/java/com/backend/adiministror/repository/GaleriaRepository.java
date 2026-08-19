9ad9a808b54995880a311021fb9534fd31baa664
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-08-19 13:26:32 -0300

    feat: galeriaService, crud inicial, mas metodos auxilisares

--- src/main/java/com/backend/adiministror/repository/GaleriaRepository.java
+++ src/main/java/com/backend/adiministror/repository/GaleriaRepository.java
@@ -4,8 +4,15 @@ import com.backend.adiministror.model.GaleriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GaleriaRepository extends JpaRepository<GaleriaModel, UUID> {
-    List<GaleriaModel> findByNomeIgnoreCase(String nome);
+    List<GaleriaModel> findByNomeContainingIgnoreCase(String nome);
+
+    List<GaleriaModel> findByDono_Id(UUID id);
+
+    List<GaleriaModel> findByEnderecoCidadeIgnoreCase(String cidade);
+
+    //boolean existsByDonoIdAndNomeIgnoreCase(UUID donoId, String nome); //para evitar que o mesmo usuário crie galerias com o mesmo nome
 }
--- src/main/java/com/backend/adiministror/service/GaleriaService.java
+++ src/main/java/com/backend/adiministror/service/GaleriaService.java
@@ -1,4 +1,140 @@
 package com.backend.adiministror.service;
 
+import com.backend.adiministror.dto.GaleriaRequest;
+import com.backend.adiministror.dto.GaleriaResponse;
+import com.backend.adiministror.model.EnderecoModel;
+import com.backend.adiministror.model.GaleriaModel;
+import com.backend.adiministror.model.UsuarioModel;
+import com.backend.adiministror.repository.GaleriaRepository;
+import com.backend.adiministror.repository.SalasRepository;
+import com.backend.adiministror.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class GaleriaService {
+    private final GaleriaRepository galeriaRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final CurrentUserService currentUserService;
+    private final SalasRepository salasRepository;
+
+    public GaleriaResponse create(GaleriaRequest request){
+        UUID usuarioAtual = currentUserService.getCurrentUserId();
+
+        UsuarioModel dono = usuarioRepository.findById(usuarioAtual)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        EnderecoModel endereco = new EnderecoModel(
+                request.endereco().zipCode(),
+                request.endereco().estado(),
+                request.endereco().cidade(),
+                request.endereco().bairro(),
+                request.endereco().rua(),
+                request.endereco().numero(),
+                request.endereco().complemento()
+        );
+
+        GaleriaModel galeria = new GaleriaModel(
+                request.nome().trim(),
+                request.phone(),
+                endereco,
+                dono
+        );
+
+        return GaleriaResponse.from(galeriaRepository.save(galeria));
+    }
+
+    public GaleriaResponse update(UUID id, GaleriaRequest request){
+        UUID usuarioAtal = currentUserService.getCurrentUserId();
+
+        UsuarioModel dono = usuarioRepository.findById(usuarioAtal)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        GaleriaModel galeria = galeriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(
+                        "Galeria não encontrada"
+                ));
+
+        EnderecoModel endereco = new EnderecoModel(
+                request.endereco().zipCode(),
+                request.endereco().estado(),
+                request.endereco().cidade(),
+                request.endereco().bairro(),
+                request.endereco().rua(),
+                request.endereco().numero(),
+                request.endereco().complemento()
+        );
+
+        galeria.atualizarDados(
+                request.nome().trim(),
+                request.phone()
+        );
+
+        return GaleriaResponse.from(galeriaRepository.save(galeria));
+    }
+
+    public List<GaleriaResponse> buscarGalerias(String nome){
+        if (nome == null || nome.trim().isEmpty()){
+            throw new RuntimeException("Digite um nome para buscar");
+        }
+
+        return galeriaRepository.findByNomeContainingIgnoreCase(nome.trim())
+                .stream()
+                .map(GaleriaResponse::from)
+                .toList();
+    }
+
+    public void delete(UUID id){
+        GaleriaModel galeria = buscarGaleriaDoUsuario(id);
+        galeriaRepository.delete(galeria);
+    }
+
+    public GaleriaResponse buscar(UUID id) {
+
+        GaleriaModel galeria = buscarGaleriaDoUsuario(id);
+        return GaleriaResponse.from(galeria);
+    }
+
+    public long contarSalas(UUID galeriaId) {
+        if (!galeriaRepository.existsById(galeriaId)) {
+            throw new RuntimeException("Galeria não encontrada");
+        }
+        return salasRepository.countByGaleriaId(galeriaId);
+    }
+    public List<GaleriaResponse> buscarMinhasSlas() {
+        UUID usuarioAtual = currentUserService.getCurrentUserId();
+
+        return galeriaRepository.findByDono_Id(usuarioAtual)
+                .stream()
+                .map(GaleriaResponse::from)
+                .toList();
+    }
+
+    public List<GaleriaResponse> buscarTodasGalerias(String cidade) {
+        if (cidade == null || cidade.trim().isEmpty()){
+            throw new RuntimeException("Digite um cidade para buscar");
+        }
+
+        return galeriaRepository.findByEnderecoCidadeIgnoreCase(cidade)
+                .stream()
+                .map(GaleriaResponse::from)
+                .toList();
+    }
+
+    private GaleriaModel buscarGaleriaDoUsuario(UUID galeriaId) {
+        UUID usuarioAtual = currentUserService.getCurrentUserId();
+
+        GaleriaModel galeria = galeriaRepository.findById(galeriaId)
+                .orElseThrow(() -> new RuntimeException("Galeria não encontrada"));
+
+        if (!galeria.getDono().getId().equals(usuarioAtual)) {
+            throw new RuntimeException("Você não tem permissão para acessar esta galeria");
+        }
+
+        return galeria;
+    }
 }
